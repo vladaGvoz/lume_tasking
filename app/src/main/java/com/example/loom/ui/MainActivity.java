a334f674fa1cc6e270b7633d9fd8beec53c8d8de
vladaGvoz <vladagvozdenovic1805@gmail.com>
2025-12-08 09:43:24 +0100

    mitko X6.1

--- app/src/main/java/com/example/loom/ui/MainActivity.java
+++ app/src/main/java/com/example/loom/ui/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // git git check check
         // repository name check
+        //one more time
     }
 }
